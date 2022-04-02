1f83ac31718d625af202fe16a7afeb1ed1493133
maurifcarmo <mauri_vrb@hotmail.com>
2022-04-02 11:07:37 -0300

    Improve naming consistency

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = require('./config/router')
 require('./config/dbConnect')
-require('./user/auth-strategies')
+require('./user/authStrategies')
 require('dotenv').config()
 
 const app = express()
