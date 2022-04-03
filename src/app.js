8f4d82c6cd6b06c76f96fc64420c982a096fc948
maurifcarmo <mauri_vrb@hotmail.com>
2022-04-03 13:14:25 -0300

    Fix env no loading on vault.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const router = require('./config/router')
-const errorHandling = require('./errorHandling')
+require('dotenv').config()
 require('./config/dbConnect')
 require('./config/vault')
 require('./user/authStrategies')
-require('dotenv').config()
+const express = require('express')
+const router = require('./config/router')
+const errorHandling = require('./errorHandling')
 
 const app = express()
 
